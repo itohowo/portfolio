818095feedeff6b90152f141f0fafc28a0273b55
Mooli Morano <moolipit@gmail.com>
2016-03-01 11:39:07 +0000

    first commit

---
+++ config.rb
@@ -0,0 +1,15 @@
+
+require "susy"
+require "breakpoint"
+
+project_type = :stand_alone
+http_path = "/"
+sass_dir = "components/sass"
+css_dir = "development/stylesheets"
+images_dir = "development/images"
+fonts_dir = "fonts"
+javascripts_dir = "components/script"
+line_comments = false
+preferred_syntax = :scss
+output_style = :expanded
+relative_assets = true
