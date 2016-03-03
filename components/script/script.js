26991ba408c1423f55953333bff557852049d0f0
Mooli88 <moolipit@gmail.com>
2016-03-03 19:02:16 +0000

    Add: better blur effect, smoth scrolling and some small style changes

--- components/script/script.js
+++ components/script/script.js
@@ -2,11 +2,43 @@ var $ = require('jquery');
 
 $(function(){
 
-	var winheight = $(window).height(); //get window height
+	//get window height
+	var winheight = $(window).height(); 
 
 	$('.fullheight').css('height', winheight);	
 	$(window).resize(function(){
 		var winheight = $(window).height();
 		$('.fullheight').css('height', winheight);
 	})
+
+//blur effect
+	var $blur = $('.tiles a img');
+
+		$blur.mouseenter(function(){
+			$blur.removeClass('deactivate');
+			$blur.addClass('active');
+			$(this).removeClass('active')
+
+			$blur.mouseleave(function(){
+				$blur.addClass('deactivate');
+				
+			})
+		})
+
+	$(function() {
+  $('a[href*="#"]:not([href="#"])').click(function() {
+    if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
+      var target = $(this.hash);
+      target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+      if (target.length) {
+        $('html, body').animate({
+          scrollTop: target.offset().top
+        }, 1000);
+        return false;
+	      }
+	    }
+	  });
+	});
+
+
 });
