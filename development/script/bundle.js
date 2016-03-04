53406eac30fb86994fc1ee42822a275c9df57f99
Mooli88 <moolipit@gmail.com>
2016-03-04 22:54:02 +0000

    add more animation

--- development/script/bundle.js
+++ development/script/bundle.js
@@ -20640,9 +20640,9 @@ $(function(){
 	})
 
 //ScrollMagic
-
+var controller = new ScrollMagic.Controller();
 	//side nav follow
-	var controller = new ScrollMagic.Controller();
+	
 
 	var scene = new ScrollMagic.Scene({
 		triggerElement: '.tiles',
@@ -20655,32 +20655,66 @@ $(function(){
 
 	//side nav slide in effect
 
-	var side_nav = TweenLite.from('.side_nav', 0.9, {opacity:.5, width:0});
+	var side_nav = function(){
+		TweenLite.from('.side_nav', 0.9, {opacity:.5, width:0});
+		TweenLite.from('.side_nav ul', 1, {opacity:0, y:100, delay:0.7});
+	}
+
 	var scene = new ScrollMagic.Scene({
 		triggerElement: '#projects',
 		offset: 0,
 		triggerHook:1
 	}).setTween(side_nav).addTo(controller);	
 
-	var side_nav_ul = TweenLite.from('.side_nav ul', 1, {opacity:0, y:100, delay:0.7});
-	var scene = new ScrollMagic.Scene({
-		triggerElement: '#projects',
-		offset: 0,
-		triggerHook:1
-	}).setTween(side_nav_ul).addTo(controller);	
-
 //
 
+	//Class Toggles
+new ScrollMagic.Scene({triggerElement: "#about", triggerHook:0.5})
+		.setClassToggle("#toabout", "marked") 
+		.addTo(controller);
+	new ScrollMagic.Scene({triggerElement: "#projects", triggerHook:0.3, offset:-270, duration:1100})
+		.setClassToggle("#toprojects", "marked") // add class toggle
+
+		.addTo(controller);
+	
+	new ScrollMagic.Scene({triggerElement: "#contact adress",offset:2000 })
+		.setClassToggle("#tocontact", "marked") // add class toggle
+
+		.addTo(controller);
 
 
+		//nav transision hotpink
+		var side_links = $('.side_nav ul li a');
+		$(side_links).mouseenter(function(){
+			$(this).addClass('unmarked');
+			$(this).mouseleave(function(){
+				$(this).removeClass('unmarked');
+			})
+		})
 
 
+	//contact pop up
 
+		var side_contactBtm = document.getElementById("tocontact");
+		var main_contactBtm = document.getElementById("mainTocontact");
 
+		side_contactBtm.onclick = function(){
+			TweenLite.to('#contact', 2, {ease: Back.easeOut.config(1.7), padding:50, delay:0.2});
+			TweenLite.to('#contact address', 1, {ease: Sine.easeOut, padding:30, opacity:1, delay:1.5});
+		}
+		main_contactBtm.onclick = function(){
+			TweenLite.to('#contact', 2, {ease: Back.easeOut.config(1.7), padding:50, delay:1.6});
+			TweenLite.to('#contact address', 1, {ease: Sine.easeOut, padding:30, opacity:1, delay:1.8});
+		}
 
+		var main_nav = TweenLite.fromTo("#nav", 1.3,{y: 35},{y:160, opacity:0, ease: Back.easeOut})
 
-
-
+		new ScrollMagic.Scene({
+		triggerElement: '#nav',
+		triggerHook:0,
+		offset:10,
+		// duration:300 causing low fps
+	}).setTween(main_nav).setPin("#nav").addTo(controller);	
 
 
 
