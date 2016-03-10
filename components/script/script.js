1758b91151e39ce96a500f52b13fc6e90efdf53c
Mooli88 <moolipit@gmail.com>
2016-03-10 10:00:33 +0000

    trying to solve the scrollmagic resize problem

--- components/script/script.js
+++ components/script/script.js
@@ -8,6 +8,22 @@ var animation_gsap = require('./plugin/animation.gsap.js');
 var debug_addIndicators = require('./plugin/debug.addIndicators.js');
 //function ready below!!!
 
+
+//ScrollMagic vars
+	var controller = new ScrollMagic.Controller();
+	var	scene = new ScrollMagic.Scene();
+	
+	//second_nav slide in effect
+	var scndNavXslid = function(){
+			TweenLite.fromTo('#secondary_nav', 0.6, {opacity:.3, width:0 }, {opacity:1, width:130, delay:0.4, ease:Elastic.easeOut});
+			TweenMax.staggerFrom(".btn", 2, {x:-100, scale:0, opacity:0, delay:0.9, ease:Elastic.easeOut}, 0.2);
+			}//function 
+	var scndNavYslid = function(){
+			TweenLite.from('#secondary_nav', 1.3, {opacity:.3, height:0,delay:0.5, ease:Elastic.easeOut});
+			TweenMax.staggerFrom(".btn", 2, {y:-50, scale:0, opacity:0, delay:0.9, ease:Elastic.easeOut}, 0.3);
+			}//function
+
+	
 	//get window height
 function fullheight(selector, tims){
 	var winheight = $(window).height(); 
@@ -25,61 +41,20 @@ function checkSize(){
   if ($("#secondary_nav").css("float") == "right" ){
   	var winheight = $(window).height();
     $('#secondary_nav').css('height', winheight * 2);	
+
 		$( "#secondary_nav" ).switchClass( "wide", "narrow");
+		
 	}//if
 	else if ($("#secondary_nav").css("float") == "none" ){
 		$('#secondary_nav').removeAttr("style");
+
 		$( "#secondary_nav" ).switchClass( "narrow", "wide");
+
 	}//else
 }//checkSize
 
 
-
-		function test(){
-		if ($("#secondary_nav").css("float") == "right" ){
-			 	var	scene = new ScrollMagic.Scene({
-					triggerElement: '#projects',
-					offset: -10,
-					triggerHook:0
-				}).setPin('.narrow ul').addTo(controller)
-				$detach(".scrollmagic-pin-spacer")
-	}//if
-	else if ($("#secondary_nav").css("float") == "none" ){
-			$detach(".scrollmagic-pin-spacer")
-				var	scene = new ScrollMagic.Scene({
-					triggerElement: '#secondary_nav',
-					offset: 0,
-					triggerHook:0
-				}).setPin('.wide').addTo(controller);		
-				$detach(".scrollmagic-pin-spacer")
-		}//else
-	}//checkSize
-
-
-
-
-
-
-
-
-//ScrollMagic
-	var controller = new ScrollMagic.Controller();
-	var	scene = new ScrollMagic.Scene();
-		
-	//second_nav slide in effect
-	var scndNavXslid = function(){
-				TweenLite.fromTo('#secondary_nav', 0.6, {opacity:.3, width:0 }, {opacity:1, width:130, delay:0.4, ease:Elastic.easeOut});
-				TweenMax.staggerFrom(".btn", 2, {x:-100, scale:0, opacity:0, delay:0.9, ease:Elastic.easeOut}, 0.2);
-				}//function 
-	var scndNavYslid = function(){
-					TweenLite.from('#secondary_nav', 1.3, {opacity:.3, height:0,delay:0.5, ease:Elastic.easeOut});
-					TweenMax.staggerFrom(".btn", 2, {y:-50, scale:0, opacity:0, delay:0.9, ease:Elastic.easeOut}, 0.3);
-					}//function
-
-	
-
-
-//document ready//
+//++document ready++//
 $(function(){
 
 
@@ -90,18 +65,41 @@ $(function(){
 	//secondary nav heigh
 		 // run test on initial page load
     checkSize();
-test();
+
     // run test on resize of the window
     $(window).resize(checkSize);
-    $(window).resize(test);
+
  
  	//ScrollMagic Pin:
 
- // 	var	scene = new ScrollMagic.Scene({
-	// 	triggerElement: '#projects',
-	// 	offset: -10,
+// function scrollPin(){
+
+//   if ($("#secondary_nav").css("float") == "right" ){
+// 		wide.removePin(true);
+// 		narrowAddPin//secondary_nav narrow add pin
+		
+// 	}//if
+// 	else if ($("#secondary_nav").css("float") == "none" ){
+// 		narrow.removePin(true);//secondary_nav removePin
+// 		wideAddPin//secondary_nav wide add pin
+// 	}//else
+// }//scrollPin
+
+
+// $(window).resize(scrollPin)
+
+
+
+
+
+
+
+
+	// var	scene = new ScrollMagic.Scene({
+	// 	triggerElement: '#nav',
+	// 	offset: 0,
 	// 	triggerHook:0
-	// }).setPin('.narrow ul').addTo(controller)
+	// }).setPin('#secondary_nav').addIndicators().addTo(controller)
 
 	// var	scene = new ScrollMagic.Scene({
 	// 	triggerElement: '#secondary_nav',
